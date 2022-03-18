eae0cb9a0bcb5891be2423276434ac1d8ab6d5e9
dubicube <dubreuil.20100@gmail.com>
2022-03-19 00:33:53 +0100

    Hot fix 4

--- src/main.py
+++ src/main.py
@@ -75,7 +75,7 @@ def runPybrenda(command):
 def highLevelTextCallback(contextual_bot, sh_core):
     msg = contextual_bot.getText()
     rep = ""
-    if pybrenda_enabled and (contextual_bot.getChatID() == -1001216704238 or contextual_bot.getChatID() == conv_perso or contextual_bot.getChatID() == -1001459505391):
+    if pybrenda_enabled and (contextual_bot.getChatID() in [-1001168293232, -1001216704238, conv_perso, -1001459505391]):
         rep = runPybrenda(bytes(msg, 'utf8'))
         if "<function" in rep and len(msg.split(' ')) == 1:
             rep = runPybrenda(bytes(msg+"()", 'utf8'))
