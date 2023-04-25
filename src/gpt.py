f89e0e1826308f31db4bb74d31f2be5e0ee47184
dubicube <dubreuil.20100@gmail.com>
2023-04-25 23:34:34 +0200

    Bug fix

--- src/gpt.py
+++ src/gpt.py
@@ -2,13 +2,13 @@ import openai
 
 from contextual_bot import *
 
-GPT_SYSTEM_PROMPT = [{"role": "system", "content": "Tu est un chatbot qui s'appelle Brenda"}]
+GPT_SYSTEM_PROMPT = {"role": "system", "content": "Tu est un chatbot qui s'appelle Brenda"}
 # GPT_SYSTEM_PROMPT = []
 gpt_convData = {}
 
 def GPT_checkChatIDExists(chatID):
     if not chatID in gpt_convData:
-        gpt_convData[chatID] = (False, GPT_SYSTEM_PROMPT)
+        gpt_convData[chatID] = (False, [GPT_SYSTEM_PROMPT.copy()])
 
 def GPT_startConvMode(contextual_bot, sh_core):
     chatID = contextual_bot.getChatID()
@@ -16,15 +16,15 @@ def GPT_startConvMode(contextual_bot, sh_core):
     prompt = "" # TODO
     gpt_conversation_mode = True
     if len(prompt) != 0:
-        gpt_convData[chatID] = (False, GPT_SYSTEM_PROMPT + [{"role": "system", "content": prompt}])
+        gpt_convData[chatID] = (False, [GPT_SYSTEM_PROMPT.copy()] + [{"role": "system", "content": prompt}])
     else:
-        gpt_convData[chatID] = (True, GPT_SYSTEM_PROMPT)
+        gpt_convData[chatID] = (True, [GPT_SYSTEM_PROMPT.copy()])
     contextual_bot.reply(ContextualBot.TEXT, "ChatGPT mode conversation activé: tous les messages sont désormais répondu par ChatGPT (avec mémoire). Exécuter la commande /gptstop pour arrêter ce mode.")
 
 def GPT_stopConvMode(contextual_bot, sh_core):
     chatID = contextual_bot.getChatID()
     GPT_checkChatIDExists(chatID)
-    gpt_convData[chatID] = (False, GPT_SYSTEM_PROMPT)
+    gpt_convData[chatID] = (False, [GPT_SYSTEM_PROMPT.copy()])
     contextual_bot.reply(ContextualBot.TEXT, "ChatGPT mode conversation désactivé")
 
 def GPT_setSystemPrompt(contextual_bot, sh_core):
@@ -32,7 +32,7 @@ def GPT_setSystemPrompt(contextual_bot, sh_core):
     chatID = contextual_bot.getChatID()
     GPT_checkChatIDExists(chatID)
     (conversationMode, convHistory) = gpt_convData[chatID]
-    GPT_SYSTEM_PROMPT = [{"role": "system", "content": msg}]
+    GPT_SYSTEM_PROMPT = {"role": "system", "content": msg}
     convHistory[0] = {"role": "system", "content": msg}
 
 def GPT_getResponse(contextual_bot):
